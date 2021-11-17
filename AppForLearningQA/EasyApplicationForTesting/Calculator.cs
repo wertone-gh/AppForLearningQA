6c00a82acd324c5eb56bbada676c86d997818971
Mateusz Stolecki <mateusz.stolecki@outlook.com>
2021-11-17 17:55:47 +0100

    Added simple test application

---
+++ AppForLearningQA/EasyApplicationForTesting/Calculator.cs
@@ -0,0 +1,15 @@
+﻿namespace EasyApplicationForTesting;
+
+internal sealed class Calculator
+{
+    public double Add(double x, double y) => x + y;
+
+    //Mistake on purpose
+    public double Sub(double x, double y) => x + y;
+
+    public double Mul(double x, double y) => x * y;
+
+    //Note: Does not check divide by zero condition here.
+    //This bug should be found by QA while training using this application
+    public double Div(double x, double y) => x / y;
+}
---
+++ AppForLearningQA/EasyApplicationForTesting/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+
+namespace EasyApplicationForTesting;
+
+/// <summary>
+///     Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private readonly Calculator _calculator;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        _calculator = new Calculator();
+    }
+
+    //Validation is ignored for testing purposes. QA should find bugs here during his learning process..
+    private void addButton_Click(object sender, RoutedEventArgs e)
+    {
+        var (firstNumber, secondNumber) = ParseNumbers(numberOneTextBox.Text, numberTwoTextBox.Text);
+        resultTextBox.Text = _calculator.Add(firstNumber, secondNumber).ToString();
+    }
+
+    private void subButton_Click(object sender, RoutedEventArgs e)
+    {
+        var (firstNumber, secondNumber) = ParseNumbers(numberOneTextBox.Text, numberTwoTextBox.Text);
+        resultTextBox.Text = _calculator.Sub(firstNumber, secondNumber).ToString();
+    }
+
+    private void mulButton_Click(object sender, RoutedEventArgs e)
+    {
+        var (firstNumber, secondNumber) = ParseNumbers(numberOneTextBox.Text, numberTwoTextBox.Text);
+        resultTextBox.Text = _calculator.Mul(firstNumber, secondNumber).ToString();
+    }
+
+    private void divButton_Click(object sender, RoutedEventArgs e)
+    {
+        var (firstNumber, secondNumber) = ParseNumbers(numberOneTextBox.Text, numberTwoTextBox.Text);
+        resultTextBox.Text = _calculator.Div(firstNumber, secondNumber).ToString();
+    }
+
+    private (double firstNumber, double secondNumber) ParseNumbers(string numberOneText, string numberTwoText)
+    {
+        //Those line can fail if tester input string that is not numeric or empty. This is done on purpose for testing course purposes.
+        var firstNumber = double.Parse(numberOneTextBox.Text);
+        var secondNumber = double.Parse(numberTwoTextBox.Text);
+        return (firstNumber, secondNumber);
+    }
+}
